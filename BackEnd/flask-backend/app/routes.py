f59cdff9fde136d3b249cd3c64224ec84e4ec9ff
PommesPeter <434596665@qq.com>
2021-06-15 12:02:50 +0800

    added code of connecting database

--- BackEnd/flask-backend/app/routes.py
+++ BackEnd/flask-backend/app/routes.py
@@ -2,6 +2,8 @@
 from app import app
 from flask import render_template
 
+import psycopg2
+
 
 # 建立路由，通过路由可以执行其覆盖的方法，可以多个路由指向同一个方法。
 @app.route('/')
@@ -13,3 +15,35 @@ def index():
 @app.route('/error')
 def error_page():
     return render_template('404.html')
+
+
+@app.route('/stu/stuLogin')
+def login(name, passwd):
+    userid = ""
+    conn = psycopg2.connect(database="postgres", user="postgres",
+                            password="postgres", host="192.168.1.200", port="5432")
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE test_conn(id int, name text)")
+    cursor.execute("INSERT INTO test_conn values(1,'haha')")
+    # 提交SQL命令
+    conn.commit()
+
+    cursor.execute("select * from test_conn")
+
+    # 获取SELECT返回的元组
+    rows = cursor.fetchall()
+    for row in rows:
+        print('id = ', row[0], 'name = ', row[1], '\n')
+
+    # 关闭游标
+    cursor.close()
+
+    # 关闭数据库连接
+    conn.close()
+
+    return userid
+
+
+@app.route("/stu/getStuInfo")
+def getStuInfo():
+    pass
