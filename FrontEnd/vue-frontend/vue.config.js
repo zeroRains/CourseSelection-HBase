efab7b08bcd2b8d2a46b40c7c07769bcdeb9163f
zeroRains <2274033547@qq.com>
2021-06-18 10:50:21 +0800

    update 数据库说明文档

--- FrontEnd/vue-frontend/vue.config.js
+++ FrontEnd/vue-frontend/vue.config.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//     devServer: {
-//         proxy: {
-//             '/': {
-//                 target: 'http://10.33.0.134:5000/', //接口域名
-//                 changeOrigin: true,             //是否跨域
-//                 ws: true,                       //是否代理 websockets
-//                 // secure: true,                   //是否https接口
-//                 pathRewrite: {                  //路径重置
+module.exports = {
+    devServer: {
+        proxy: {
+            '/': {
+                target: 'http://10.33.0.134:5000/', //接口域名
+                changeOrigin: true,             //是否跨域
+                ws: true,                       //是否代理 websockets
+                // secure: true,                   //是否https接口
+                pathRewrite: {                  //路径重置
                    
-//                 }
-//             }
-//         }
-//     }
-// };
+                }
+            }
+        }
+    }
+};
