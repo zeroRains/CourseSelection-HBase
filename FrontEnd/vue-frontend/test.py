8795ff4be9eda426ce99f53d04fb4226bde6d3c0
zeroRains <2274033547@qq.com>
2021-06-19 11:02:18 +0800

    课程计划更新

--- FrontEnd/vue-frontend/test.py
+++ FrontEnd/vue-frontend/test.py
@@ -1,6 +1,6 @@
 import requests
 path = "http://10.33.14.231:5000/"
 userid = "3b598a74-a3e4-3a14-9262-eb05fc90fbfd"
-t = requests.get(path+'all/getCourseTable')
+t = requests.get(path+'all/getCourseScheduleTable')
 print(t.text)
 print(t)
