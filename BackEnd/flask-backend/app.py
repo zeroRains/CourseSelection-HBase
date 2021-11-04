00343ac2df10e5f16a178846a4d77fde2f9db672
zeroRains <2274033547@qq.com>
2021-11-04 16:15:29 +0800

    -mwhatever

--- BackEnd/flask-backend/app.py
+++ BackEnd/flask-backend/app.py
@@ -1,6 +1,7 @@
 # 从app模块中导入app应用
 from app import app
-
+import os
 # 防止被引用后执行，只有在当前模块中才可以使用
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0',port=9999)
+    app.run(debug=False, host='0.0.0.0', port=9999)
+# 随便改点东西
